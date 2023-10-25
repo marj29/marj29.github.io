366909f5b522cc48c7c19d37c7796af63405091d
marj29 <margiealviola@ymail.com>
2023-10-25 20:26:51 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const body = document.body
+body.append("Web Dev Workshop")
+
+// const firstNum = 64
+// const secondNum = 32
+// const sum = firstNum+secondNum
+
+// // console.log(subtraction(firstNum,secondNum));
+// // console.log(multiplication(firstNum,secondNum));
+// // console.log(division(firstNum,secondNum));
+
+// let i = 100
+// while(i>1){
+//     if(i%2 === 0)
+//     {
+//         console.log(i);
+//     }
+//     i--
+// }
+// // const number = 100
+
+// // for(let i = 1; i<100; i++){
+// //     if(i%2!==0)
+// //     {
+// //         console.log(i);
+// //     }
+// // }
+// // for(let i = 10; i<=1000;){
+// //     console.log(i);
+// //     i=i+10
+// // }
+
+
+// // const age = 12
+
+// // if(age >= 12){
+// //     alert("Children")
+// // }else if(age >= 13 && age <=21){
+// //     alert("Teenager")
+// // }else if(age >=22 && age <=59){
+// //     alert("Adult")
+// // }else if(age >= 60){
+// //     alert("Senior")
+// // }else{
+// //     alert("Undefined Age")
+// // }
+
+
+// function subtraction(firstnum, secondnum){
+//     return firstNum - secondNum;
+// }
+
+// function multiplication(firstnum, secondnum){
+//     return firstNum * secondNum;
+// }
+
+// function division(firstnum, secondnum){
+//     return firstNum / secondNum;
+// }
